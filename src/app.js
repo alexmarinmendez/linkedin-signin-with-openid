d226e001856446b436a877aea33759ffe3492897
Alex Marin Mendez <alexmarinmendez@gmail.com>
2024-03-29 16:05:38 -0500

    chore: change static and hbs folders in app.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import handlebars from 'express-handlebars'
 import session from 'express-session'
+import {fileURLToPath} from 'url'
 import { dirname } from 'path'
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
